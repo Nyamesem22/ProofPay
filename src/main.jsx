955e93c9eb41c8a44c687fe935f4a0e449af7154
Nyamesem Gracious <eaduboahene39@gmail.com>
2026-09-16 17:30:01 +0000

    Use completed ProofPay mobile app on mobile route

--- src/main.jsx
+++ src/main.jsx
@@ -1,14 +1,14 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
 import { App } from "./App.jsx";
-import { MobileApp } from "./MobileApp.jsx";
+import { MobileFinal } from "./MobileFinal.jsx";
 import "./styles.css";
 
 const isMobileApp = window.location.pathname.startsWith("/mobile");
 
 createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    {isMobileApp ? <MobileApp /> : <App />}
+    {isMobileApp ? <MobileFinal /> : <App />}
   </React.StrictMode>,
 );
 
