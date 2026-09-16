40a46a8ae94cd8810358daa2afdfcadcddbb221a
Nyamesem Gracious <eaduboahene39@gmail.com>
2026-09-16 16:48:56 +0000

    Route /mobile to ProofPay mobile app

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,13 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
 import { App } from "./App.jsx";
+import { MobileApp } from "./MobileApp.jsx";
 import "./styles.css";
+
+const isMobileApp = window.location.pathname.startsWith("/mobile");
 
 createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
+    {isMobileApp ? <MobileApp /> : <App />}
   </React.StrictMode>,
 );
